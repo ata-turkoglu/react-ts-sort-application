ef98f4273b95727f962a0c598a4cb66e8e2f805d
ata_turkoglu <ata.turkoglu@reengen.com>
2023-11-09 20:20:17 +0300

    add ts files

--- build/index.js
+++ build/index.js
@@ -1,3 +1,21 @@
 "use strict";
-class SortData {
-}
+Object.defineProperty(exports, "__esModule", { value: true });
+const Numbers_1 = require("./Numbers");
+const Strings_1 = require("./Strings");
+const dateArray = [
+    new Date(2200, 9, 24),
+    new Date(2035, 9, 24),
+    new Date(2041, 9, 24),
+    new Date(2029, 9, 24),
+];
+const years = dateArray.map((date) => {
+    return date.getFullYear();
+});
+const sortYears = new Numbers_1.Numbers(years);
+console.log("****", sortYears);
+sortYears.sort();
+console.log("***1", sortYears);
+console.log("***2", sortYears.number);
+const stringsData = new Strings_1.Strings("vdGryF");
+stringsData.sort();
+console.log(stringsData.str);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-class SortData{}
+import { Numbers } from "./Numbers";
+import { Strings } from "./Strings";
+
+const dateArray = [
+    new Date(2200, 9, 24),
+    new Date(2035, 9, 24),
+    new Date(2041, 9, 24),
+    new Date(2029, 9, 24),
+];
+
+const years = dateArray.map((date) => {
+    return date.getFullYear();
+});
+
+const sortYears = new Numbers(years);
+
+console.log("****", sortYears);
+sortYears.sort();
+console.log("***1", sortYears);
+console.log("***2", sortYears.number);
+
+const stringsData = new Strings("vdGryF");
+stringsData.sort();
+console.log(stringsData.str);
